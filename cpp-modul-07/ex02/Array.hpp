0c55ceb880463f3c20b06dd6c106a9121254f205
Mohamed Amine Amazzal <mamazzal@c1r2p10.1337.ma>
2023-11-22 21:34:24 +0100

    init

--- cpp-modul-07/ex02/Array.hpp
+++ cpp-modul-07/ex02/Array.hpp
@@ -5,22 +5,23 @@
 
 template <typename T>
 class Array {
-    private :
-      T *array;
-      unsigned int arrSize;
-    public :
-      Array();
-      Array(Array const &src);
-      Array &operator=(Array const &src);
-      Array(unsigned int n);
-      ~Array();
-      T & operator[](unsigned int i);
-      class OutOfLimitsException : public std::exception {
-        const char *what() const throw() {
-          return "Out of limits";
-        }
-      };
-      unsigned int size() const;
-  };
+  private :
+    T *array;
+    unsigned int arrSize;
+  public :
+    Array();
+    Array(Array const &src);
+    Array &operator=(Array const &src);
+    Array(unsigned int n);
+    ~Array();
+    unsigned int size() const;
+
+    T & operator[](unsigned int i);
+    class OutOfLimitsException : public std::exception {
+      const char *what() const throw() {
+        return "Out of limits";
+      }
+    };
+};
 
 #endif
