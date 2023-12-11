bd4e3cadea6cf8866ff5880e0c02381b0df9d205
Mohamed Amine Amazzal <mamazzal@c1r1p2.1337.ma>
2023-12-11 17:43:25 +0100

    init

--- test9.cpp
+++ test9.cpp
@@ -30,93 +30,47 @@ class FJA {
       }
       std::cout << ";\n";
     }
-
-    void createPairs(std::vector<int> & vec, std::vector<std::vector<int> > & newData, int index) {
-      if (index >= vec.size() - 1)
-        return;
-      int  first = vec[index];
-      int  secnd = vec[index * 1];
-      std::vector<int>  temp;
-      temp.push_back(first);
-      temp.push_back(secnd);
-      std::sort(temp.begin(), temp.end());
-      newData.push_back(temp);
-      createPairs(vec, newData, index * 2);
-      // mergePairs(newData);
-    }
-
-    void mergePairs(std::vector<std::vector<int> > & newData) {
-      std::vector<int> temp;
-      std::vector<int> next;
-
-      std::vector<std::vector<int> >::iterator parrentStart = newData.begin();
-      for(; parrentStart != newData.end(); parrentStart++) {
-          if (parrentStart + 1 == newData.end())
-            break;
-          std::vector<int> & temp = *parrentStart;
-          std::vector<int> & next = *(parrentStart + 1);
-          if (temp[1] > next[0]) {
-            std::swap(temp[1], next[0]);
-            newData.erase(parrentStart + 1);
-          }
+    void displayNumbers(std::vector<int> & gg) {
+      for(std::vector<int>::iterator start = gg.begin(); start < gg.end(); start++) {
+        std::cout << *start << " ";
       }
     }
 
     void merge() {
-        std::cout << "\n";
-        if (this->data.size() % 2 != 0) {
-          this->remain.push_back(this->data.back());
-          this->data.erase(--this->data.end());
-        }
-        std::cout << "STACK BEFOR : ";
-        this->displayNumbers();
-        turnVectorTopairs(this->data, 1);
-        std::cout << "STACK AFTER : ";
-        this->displayNumbers();
-        std::cout << "MAINCHIN : ";
-        for(int i=0; i < this->mainChine.size(); i++)
-          std::cout << mainChine[i] << " ";
-        std::cout << "\n";
-
-        std::cout << "PEND : ";
-        for(int i=0; i < this->pend.size(); i++)
-          std::cout << this->pend[i] << " ";
-        std::cout << "\n";
-
+      mergeAndSortPairs(this->data, 1);
     }
 
-
-    void turnVectorTopairs(std::vector<int> & vec, int size) {
+    void mergeAndSortPairs(std::vector<int> & vec, int size) {
       if (size * 2 > vec.size())
         return ;
-      //save the last elemnt if the size if odd 
+
       std::vector<std::pair<std::vector<int>, std::vector<int> > > pairs;
+
       std::vector<int> tmp;
       //push the odd umbers to tempory 
-      if (vec.size() % (size * 2) != 0) {
-        for(int i = vec.size() - (vec.size() % 2); i < vec.size(); i++)
-          tmp.push_back(vec[i]);
+      if (vec.size() % 2 != 0) {
+        int sizeLop = vec.size() - vec.size() % 2;
+        for(; sizeLop != vec.size(); sizeLop++)
+          tmp.push_back(vec[sizeLop]);
         vec.erase(vec.end() - (vec.size() % (size * 2)), vec.end());
       }
-
-        //fill pairs
-      for(int count = 0; count < vec.size(); count+= size*2) {
+      // this->remain = tmp;
+      int count = 0;
+      while (count < vec.size()) {          
         std::vector<int> first, secnd;
-        //   0 4 1 11 5 9 10 21 2555 23 75 26
         for(int i = count; i < count + size; i++)
           first.push_back(vec[i]);
-        for(int i = count + size; i < count + size * 2; i++)
+        for(int i = count + size; i < count + (size * 2); i++)
           secnd.push_back(vec[i]);
         if (first.back() > secnd.back())
           std::swap(first, secnd);
         pairs.push_back(std::make_pair(first, secnd));
+        count += size * 2;
       }
 
-      //clear stsack
       vec.clear();
-      //push back to stack  with new sorted numbers
-      for (size_t i = 0; i < pairs.size(); i++)
-      {
+      //// Merge sorted pairs
+      for (size_t i = 0; i < pairs.size(); i++) {
         std::vector<int>& subvector = pairs[i].first;
         std::vector<int>& ssubvector = pairs[i].second;
         for (size_t j = 0; j < size; j++)
@@ -124,10 +78,61 @@ class FJA {
         for (size_t j = 0; j < size; j++)
           vec.push_back(ssubvector[j]);
       }
-      // this->displayNumbers();
-      turnVectorTopairs(vec, size * 2);
+      mergeAndSortPairs(vec, size * 2);
+      std::vector<std::vector<int> > halfers;
+      for(int count = 0; count < vec.size(); count += size * 2) {
+        std::vector<int> f,s;
+        for(int i = count; i < count + size; i++)
+          f.push_back(vec[i]);
+        for(int i = count + size; i < count + (size * 2); i++)
+          s.push_back(vec[i]);
+        halfers.push_back(f);
+        halfers.push_back(s);
+      }
+
+      std::vector<std::vector<int> > mainChine;
+      std::vector<std::vector<int> > pendchine;
+      //push to ped and mainchine
+      std::vector<std::vector<int> >::iterator halfStart = halfers.begin();
+      int index = 0;
+      for(; halfStart != halfers.end(); halfStart++) {
+        if (index % 2 != 0)
+            mainChine.push_back(*halfStart);
+        else
+          pendchine.push_back(*halfStart);
+        index++;
+      }
+      if (tmp.size() > 0) {
+        pendchine.push_back(tmp);
+        std::cout << "PUSHED THE REMAIN TO THE TEMP" << std::endl;
+      }
+      
+      // pendchine.push_back(tmp);
+      this->displayNumbers(vec);
+      std::cout << "\n";
+      this->displayChineAndPend(mainChine, pendchine);
+    }
+
+    void displayChineAndPend(std::vector<std::vector<int> > & chine, std::vector<std::vector<int> > & pend) {
+      std::vector<std::vector<int> >::iterator chineStart = chine.begin();
+      std::vector<std::vector<int> >::iterator pendStart = pend.begin();
+      std::cout << "MAINCHINE : ";
+      for(; chineStart != chine.end(); chineStart++) {
+        std::vector<int> & temp = *chineStart;
+        for(std::vector<int>::iterator start = temp.begin(); start != temp.end(); start++)
+          std::cout << *start << " ";
+      }
+      std::cout << "\n";
+      std::cout << "PEND      : ";
+      for(; pendStart != pend.end(); pendStart++) {
+        std::vector<int> & temp = *pendStart;
+        for(std::vector<int>::iterator start = temp.begin(); start != temp.end(); start++)
+          std::cout << *start << " ";
+      }
+      std::cout << "\n";
     }
 };
+
 
 int main(int argc, char **argv) {
   int count = 1;
@@ -137,6 +142,6 @@ int main(int argc, char **argv) {
     count++;
   }
   fja.merge();
-  fja.displayNumbers();
+  // fja.displayNumbers();
   return 0;
 }
