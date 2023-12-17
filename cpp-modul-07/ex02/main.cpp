edf4c17e193544cef463ec0b9cbc262e3739c40d
Mohamed Amine Amazzal <mamazzal@c1r3p5.1337.ma>
2023-12-17 18:51:03 +0100

    fix bug

--- cpp-modul-07/ex02/main.cpp
+++ cpp-modul-07/ex02/main.cpp
@@ -6,19 +6,26 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 11:39:35 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/12/13 14:55:00 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/12/17 18:49:14 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int main() {
-  try {
-    Array<std::string>  arr(6);
-    arr[2] = "hello";
-    std::cout << arr[2] << std::endl;
-  }catch(std::exception & e) {
-    std::cout << e.what() << std::endl;
-  }
-  return 0;
+int main()
+{
+    try {
+        Array<std::string> arr(5);
+        arr[0] = "test1";
+        arr[2] = "test2";
+        arr[100000] = "sigfault";
+
+        std::cout << "arr: " << arr[0] << std::endl;
+        std::cout << "arr: " << arr[2] << std::endl;
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return (0);
 }
