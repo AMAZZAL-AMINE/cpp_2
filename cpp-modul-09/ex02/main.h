f6bbd620a00ddb05a0962a5b391cdc76b5430b36
Mohamed Amine Amazzal <mamazzal@c1r3p5.1337.ma>
2023-12-24 23:56:13 +0100

    init c++ 09

--- cpp-modul-09/ex02/main.h
+++ cpp-modul-09/ex02/main.h
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 11:39:38 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/12/19 14:33:07 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/12/24 23:55:37 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,7 @@ extern int comparizonCoundDeque;
 long satoi(std::string nbr);
 int compare(const std::vector<int> & a, const  std::vector<int> & b);
 std::vector<int> jacobsthalGenerator(int max);
-std::vector<int>  pairsToVector(std::vector<std::pair<std::vector<int>, std::vector<int> > > & pairs, size_t size);
-std::vector<std::pair<std::vector<int>, std::vector<int> > > createPairs(std::vector<int> & vec, int size);
-
-std::deque<int>  pairsToDeque(std::deque<std::pair<std::deque<int>, std::deque<int> > > & pairs, size_t size);
 std::deque<int> jacobsthalGeneratorDeque(int max);
-std::deque<std::pair<std::deque<int>, std::deque<int> > > createPairsDeque(std::deque<int> & vec, int size);
 int compareDeque(const std::deque<int> & a,const std::deque<int> & b);
 
 #endif
